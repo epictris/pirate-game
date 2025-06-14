41ab8b30621ad0b6504d2b5478fcce00c8ed46d4
isaacdonley <48499071+isaacdonley@users.noreply.github.com>
2025-06-14 18:34:26 +1000

    modular crates

--- scripts/crate.gd
+++ scripts/crate.gd
@@ -1,6 +1,15 @@
 extends RigidBody2D
 
 @export var health_component : Node = null
+@export var size2 := 20.0
+@onready var collision_shape : CollisionShape2D = CollisionShape2D.new()
+var shape : RectangleShape2D = RectangleShape2D.new()
+
+func _ready():
+	shape.size = Vector2(size2,size2)
+	collision_shape.set_shape(shape)
+	print(collision_shape.shape.size)
+	add_child(collision_shape)
 
 func take_damage(dmg):
 	health_component.take_damage(dmg)
