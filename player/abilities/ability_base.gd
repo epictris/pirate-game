24df577274c060de5e939c07c682350ce0207dd3
Tris <trispebray@gmail.com>
2025-07-04 17:10:24 +0200

    add base ability class

--- player/abilities/ability_base.gd
+++ player/abilities/ability_base.gd
@@ -1,11 +1,13 @@
 class_name AbilityBase extends Node
 
+var _active: bool = false
+
 var player: Player
 
 func _ready() -> void:
 	player = owner
 
-func update(_direction: SGFixedVector2) -> void:
+func modify(_direction: SGFixedVector2) -> void:
 	pass
 
 func activate(_direction: SGFixedVector2) -> void:
@@ -13,3 +15,9 @@ func activate(_direction: SGFixedVector2) -> void:
 
 func deactivate(_direction: SGFixedVector2) -> void:
 	pass
+
+func is_active() -> bool:
+	return _active
+
+func set_active(active: bool) -> void:
+	_active = active
