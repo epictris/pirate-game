866505c0e6dfc0b60491d934385e842ca6c77aa7
Tris <trispebray@gmail.com>
2025-07-04 20:42:09 +0200

    conver shoot bullet to use new ability system

--- player/abilities/shoot_bullet/shoot_bullet.gd
+++ player/abilities/shoot_bullet/shoot_bullet.gd
@@ -9,20 +9,18 @@ var current_direction: SGFixedVector2
 var on_cooldown: bool = false
 
 func _ready() -> void:
-	cooldown_timer.timeout.connect(_on_timeout)
 	super()
+	cooldown_timer.timeout.connect(_on_timeout)
 
-func _on_activate_input(direction: SGFixedVector2) -> void:
+func _preprocess_on_activated(direction: SGFixedVector2) -> void:
+	player.activate_ability(self)
 	current_direction = direction
-	if !player.has_active_ability():
-		player.activate_ability(self)
 
-func _on_update_input(_direction: SGFixedVector2) -> void:
+func _preprocess_on_updated(_direction: SGFixedVector2) -> void:
 	current_direction = _direction
 
-func _on_deactivate_input(_direction: SGFixedVector2) -> void:
-	if player.is_ability_active(self):
-		player.deactivate_ability(self)
+func _preprocess_on_deactivated(_direction: SGFixedVector2) -> void:
+	player.deactivate_ability(self)
 
 func _hook_after_player_movement() -> void:
 	if !on_cooldown:
